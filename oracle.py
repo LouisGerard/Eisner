95b00ad1bde304ac8b45e4eb8f75d7ae9fa41903
Louis Gerard <louis.gerardd@gmail.com>
2018-10-12 12:40:04 +0200

    refactoring oracle

--- oracle.py
+++ oracle.py
@@ -1,42 +1,47 @@
 from os import listdir
 import codecs
 
-oracle = {}
 
-data_dir = 'data/'
-lang_dir = data_dir + 'fr/'
-for filename in listdir(lang_dir):
-    with codecs.open(lang_dir + filename, encoding='utf-8') as file:
-        sentence = [('__ROOT__', None, None)]
-        ignore = False
-        line_nb = 0 ################# delete me
-        for line in file:
-            line_nb += 1 ################## delete me
-            if line.startswith('#') or len(line) == 1 or ignore:
-                if len(sentence) == 1:  # ignore comments and empty lines
-                    ignore = False
-                    continue
+class Oracle:
+    def __init__(self, data_dir='data/', langs=None):
+        if langs is None:
+            langs = ['fr']
+        self._oracle = {}
+        self.data_dir = data_dir
+        self.langs = langs
 
-                if not ignore:
-                    # new sentence
-                    for word, parent, link in sentence[1:]:
-                        if word not in oracle:
-                            oracle[word] = {}
-                        oracle[word][sentence[parent][0]] = link
-                    sentence = [('__ROOT__', None, None)]
-                continue
+    def _load(self, lang):
+        lang_dir = self.data_dir + lang
+        for filename in listdir(lang_dir):
+            with codecs.open(lang_dir + filename, encoding='utf-8') as file:
+                sentence = [('__ROOT__', None, None)]
+                ignore = False
+                line_nb = 0 ################# delete me
+                for line in file:
+                    line_nb += 1 ################## delete me
+                    if line.startswith('#') or len(line) == 1 or ignore:
+                        if len(sentence) == 1:  # ignore comments and empty lines
+                            ignore = False
+                            continue
 
-            # ASSUMPTION : words are always in the sentence order
-            line = line.split('\t')
+                        if not ignore:
+                            # new sentence
+                            for word, parent, link in sentence[1:]:
+                                if word not in self._oracle:
+                                    self._oracle[word] = {}
+                                self._oracle[word][sentence[parent][0]] = link
+                            sentence = [('__ROOT__', None, None)]
+                        continue
 
-            if line[6] == '_':  # no parent ? f*ck you then
-                ignore = True
-                continue
+                    # ASSUMPTION : words are always in the sentence order
+                    line = line.split('\t')
 
-            word = line[1]
-            parent = int(line[6])
-            link = line[7]
+                    if line[6] == '_':  # no parent ? f*ck you then
+                        ignore = True
+                        continue
 
-            sentence.append((word, parent, link))
+                    word = line[1]
+                    parent = int(line[6])
+                    link = line[7]
 
-print('test')
+                    sentence.append((word, parent, link))
